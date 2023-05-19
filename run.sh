39c8b74a6851d9a383005d21f804b7ebe93093a1
laurabpaulsen <202005791@post.au.dk>
2023-05-19 13:30:16 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -7,16 +7,14 @@ python src/scrape_songs.py
 
 # preprocess songs
 echo "[INFO]: Preparing dataframe with song lyrics..."
-python src/preprocess_songs.py
+python src/preprocess_lyrics.py
 
 # Use TextSpace to create 3d visualization of the lyrics (using both emotion and gpt2 embeddings)
-echo "[INFO]: Creating 3d visualization of the lyrics..."
-
 embeddings="emotion gpt2"
 for embedding in $embeddings; do
-    python src/textspace.py --embedding_type $embedding
+echo "[INFO]:Creating 3d visualization of the lyrics using $embedding embeddings..."
+    python src/text_space.py --embedding_type $embedding
 done
 
-python src/textspace.py
 
 
